89e27f74ae478327a41f112be402e188b49ad2b7
Juan Pablo Plúas <jppluas@espol.edu.ec>
2024-06-17 10:22:24 -0500

    update 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
+/* import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/vite.svg' */
+import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+/*   const [count, setCount] = useState(0)
+ */
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Grid container spacing={5}>
+	      <Grid xs={12} sm={4} md={3} lg={2}>1</Grid>
+	      <Grid xs={6} sm={4} md={3} lg={2}>2</Grid>
+	      <Grid xs={6} sm={4} md={3} lg={2}>3</Grid>
+	      <Grid xs={12} sm={4} md={3} lg={2}>4</Grid>
+	      <Grid xs={6} sm={4} md={6} lg={2}>5</Grid>
+	      <Grid xs={6} sm={4} md={6} lg={2}>6</Grid>
+	    </Grid>
   )
 }
 
